3b2482ff6ef6ec93a4877628262fe170ece562fe
Vivian Chu <vchu@gatech.edu>
2015-04-10 21:59:48 +0000

    made modifications to make parsing h5 files more transparent and robust to all forms of inputs

--- src/parse_bag_pytables.py
+++ src/parse_bag_pytables.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
-import roslib
-roslib.load_manifest("data_logger_bag")
+import roslib; roslib.load_manifest("data_logger_bag")
 import rospy
 import rosbag
 import numpy
@@ -28,7 +27,12 @@ class DataTableBagProcessor():
         # Intialize the node
         rospy.init_node("bag_parser", anonymous=True)
         rospy.loginfo("Initializing bag parser")
-       
+      
+        ###################################################################
+        # Read from the parameter server/input to determine
+        # what topics to process, whether to subsample, and input/outputs
+        ###################################################################
+ 
         # Default topics to skip and subsample to
         default_skip_topics = "camera/depth_registered/image_raw camera/rgb/image_raw c6_end_effect_pose_right c6_end_effect_pose_left camera/rgb/camera_info camera/depth_registered/camera_info"
         default_trigger_topic = "C6_FSM_state"
@@ -37,46 +41,52 @@ class DataTableBagProcessor():
         self.skip_topics = rospy.get_param("~skip_topics", default_skip_topics)
         input_files = rospy.get_param("~input_files", "")
         output_file = rospy.get_param("~output_file", "converted_data.h5")
-        output_file_flag = rospy.get_param("~dir_outfile_flag", False)
 
         # These need to be set if we want to subsample down to a topic
         # Otherwise by default nothing is aligned
-        self.num_joints = int(rospy.get_param("~num_joints", "57"))
         self.sub_sample = rospy.get_param("~sub_sample_flag", False)
         self.trigger_topic = rospy.get_param("~trigger_topic", default_trigger_topic)
 
+        # Actually exits the system with the usage if input files are not provided
         if input_files == "":
-            rospy.logerr("Usage: %s _input_files:=<input_files> \n Optional args:\n_output_file:=<output_file.h5>\n_skip_topics:=<ros topics to NOT parse>\n_trigger_topic:=<what ros topic to downsample to>\n_sub_sample_flag:=<True or False>\n_num_joints:=<num_joints>\n_dir_outfile_flag:=<True or False>" % sys.argv[0])
+            rospy.logerr("Usage: %s _input_files:=<input_files> \nOptional args:\n\n\
+               _output_file:=<output_file.h5>\n\
+               _skip_topics:=<ros topics to NOT parse>\n\
+               _trigger_topic:=<what ros topic to downsample to>\n\
+               _sub_sample_flag:=<True or False>\n\
+               _dir_outfile_flag:=<True or False>" % sys.argv[0])
             sys.exit()
 
-        # Check if we passed in a directory
+        ###################################################################
+        # Process input/output and subsample flags
+        ###################################################################
+
+        # Check if we passed in a directory and setup proper input/output files
         if os.path.isdir(input_files):
             self.input_filenames = input_files
-            self.directory = True
+
+            # Setup output file names with directory format
+            file_split = input_files.split(os.sep)
+            self.output_filename = file_split[-1]+'.h5'
         else:
             # Expand wildcards if any
             self.input_filenames = glob.glob(os.path.expanduser(input_files))
-            self.directory = False
+            self.output_filename = os.path.expanduser(output_file)
 
-        self.output_filename = os.path.expanduser(output_file)
-
-        if output_file_flag:
-            # Setup output file names with directory format
-            file_split = input_files.split(os.sep)
-            if self.directory:
-                self.output_filename = file_split[-1]+'.h5'
-            else:            
-                self.output_filename = "-".join(file_split[-3:-1])+'.h5'
-            rospy.loginfo("Output file name is: %s" % self.output_filename)
-
+        # Check if we're subsampling the data
         if self.sub_sample:
             self.output_filename = self.output_filename.split('.')[0] + '_subsampled.h5'
             rospy.logwarn("WARNING: subsample topic is %s. Make sure this is your state..." % self.trigger_topic)
 
-        # Initialize some variables that might be needed
+        rospy.loginfo("Output file name is: %s" % self.output_filename)
+
+        ###################################################################
+        # Some simple vars to init
+        ###################################################################
         self.fileCounter = dict()
         self.fileCounter['playback'] = 0
         self.fileCounter['learning'] = 0
+        self.bag_group = None
        
     def setup_h5(self):
 
@@ -86,36 +96,80 @@ class DataTableBagProcessor():
 
         rospy.loginfo("Initialized h5 output file")
 
-    def process_all(self):
-        if (self.directory):
+    def get_subdirs(self, a_dir):
+        '''
+        Only gets the immediate subdirs
+        '''
+        return [name for name in os.listdir(a_dir)
+                if os.path.isdir(os.path.join(a_dir, name))]
 
-            # Create a bag root with the root dir
-            root_dir_name = os.path.split(self.input_filenames)[-1]
-            root_group = self.h5file.createGroup("/", root_dir_name) 
+    def get_subfiles(self, a_dir):
+        '''
+        Only gets the immediate files 
+        '''
+        return [name for name in os.listdir(a_dir)
+                if not os.path.isdir(os.path.join(a_dir, name))]
 
-            # Currently assume top level... (3 levels in)
-            for exp_type_dir in os.listdir(self.input_filenames):
-                exp_type_loc = os.path.join(self.input_filenames,exp_type_dir)
-                exp_type_group = self.h5file.createGroup(root_group, exp_type_dir)                
+    def process_general(self):
+        '''
+        This is the main starting function that processes a directory or
+        a set of files and calls the internal recursive process to create
+        h5 groups
+        '''
+        # Check if it is a directory
+        if not isinstance(self.input_filenames, list):
+          
+            # Check if group_name is empty
+            root_name = os.path.split(self.input_filenames)[-1]
+            group_name = self.h5file.createGroup("/", root_name) 
 
-                # Go through all the users
-                for user in os.listdir(exp_type_loc):
-                    user_loc = os.path.join(exp_type_loc, user)
-                    self.bag_group = self.h5file.createGroup(exp_type_group, user)                
-                    
-                    # for all files finally
-                    filenames = []
-                    filesdir = os.listdir(user_loc) 
-                    filesdir.sort() # puts in the order listed in the OS
-                    for f in filesdir:
-                        if f.endswith(".bag"):
-                            filename = os.path.join(user_loc, f)
-                            filenames.append(filename)
-                    self.process_files(filenames)
-
-        else:
+            # Actually calls the heavy duty function 
+            self.process_all_recurse(self.input_filenames, group_name) 
             
+        else:
+            # Else just treat the input as a list of files
             self.process_files(self.input_filenames)
+
+    def process_all_recurse(self, filename, group_name):
+        '''
+        Recursively goes through the directory and files and 
+        creates the proper h5 groups and populates them
+        '''
+        dirs = self.get_subdirs(filename)
+        files = self.get_subfiles(filename)
+
+        # Write all files in the current directory
+        if files:
+            self.process_directory(filename, group_name)
+
+        # If we have more directories, recurse down
+        if dirs:
+            for directory in dirs:
+                foldername = os.path.split(directory)[-1]
+                new_group = self.h5file.createGroup(group_name, foldername)
+                self.process_all_recurse(os.path.join(filename,directory), new_group) 
+             
+
+    def process_directory(self, directory, group_name): 
+        '''
+        Internal function that takes a directory and converts it into a list
+        of files to write to a specific bag group
+        '''
+ 
+        # Create the bag group to write to
+        self.bag_group = group_name
+
+        # for all files finally
+        filenames = []
+        filesdir = os.listdir(directory) 
+        filesdir.sort() # puts in the order listed in the OS
+        for f in filesdir:
+            if f.endswith(".bag"):
+                filename = os.path.join(directory, f)
+                filenames.append(filename)
+
+        # Now actually call the main function that processes the files
+        self.process_files(filenames)
 
     def process_files(self, input_files):
 
@@ -185,6 +239,15 @@ class DataTableBagProcessor():
                 self.bagData.process_msg(topic, (msg, stamp))
 
     def create_dummy_msg(self, topic):
+        '''
+        This function is only called during subsampling.  This takes care of the possiblity
+        that certain messages have not been seen before the first "tick" of the trigger
+        topic and populates the message with NaNs.  The loading of h5 takes care of
+        processing the NaNs
+
+        Note: This section has some hard-coded messages and topic sizes that
+              might be effected drastically with changes in the robot or messages
+        '''
 
         # Custom function that will process the messages depending on type
         msg_type = self.bagData.topic_types[topic] 
@@ -325,7 +388,7 @@ def main():
     data.setup_h5()
         
     # start processing bag files
-    data.process_all()
+    data.process_general()
 
     # Close the h5 file
     data.h5file.close()
